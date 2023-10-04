4f650e844c4fc47768ae255f73cf96ccedb52d54
seyed-sabbagh <mohammadjavadsabagh@gmail.com>
2023-10-04 11:04:14 +0330

    product-detail-two top page Done

---
+++ helendo/assets/js/js.js
@@ -0,0 +1,226 @@
+function getImageDetail1() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN1').src;
+    const productContent = document.getElementById('product-content-1').text;
+    localStorage.setItem('productContent', productContent);
+
+    localStorage.setItem('imageAddress', imageAddress);
+
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+
+function getImageDetail2() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN2').src;
+
+    const productContent = document.getElementById('product-content-2').text;
+    localStorage.setItem('productContent', productContent);
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+
+function getImageDetail3() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN3').src;
+    const productContent = document.getElementById('product-content-3').text;
+    localStorage.setItem('productContent', productContent);
+
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+function getImageDetail4() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN4').src;
+
+    const productContent = document.getElementById('product-content-4').text;
+    localStorage.setItem('productContent', productContent);
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+function getImageDetail5() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN5').src;
+    const productContent = document.getElementById('product-content-5').text;
+    localStorage.setItem('productContent', productContent);
+
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+function getImageDetail6() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN6').src;
+    const productContent = document.getElementById('product-content-6').text;
+    localStorage.setItem('productContent', productContent);
+
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+function getImageDetail7() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN7').src;
+
+    const productContent = document.getElementById('product-content-7').text;
+    localStorage.setItem('productContent', productContent);
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+function getImageDetail8() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN8').src;
+
+    const productContent = document.getElementById('product-content-8').text;
+    localStorage.setItem('productContent', productContent);
+
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+function getImageDetail9() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN9').src;
+
+    const productContent = document.getElementById('product-content-9').text;
+    localStorage.setItem('productContent', productContent);
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+function getImageDetail10() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN10').src;
+
+    const productContent = document.getElementById('product-content-10').text;
+    localStorage.setItem('productContent', productContent);
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+function getImageDetail11() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN11').src;
+    const productContent = document.getElementById('product-content-11').text;
+    localStorage.setItem('productContent', productContent);
+
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+function getImageDetail12() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN12').src;
+    const productContent = document.getElementById('product-content-12').text;
+    localStorage.setItem('productContent', productContent);
+
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+function getImageDetail13() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN13').src;
+    const productContent = document.getElementById('product-content-13').text;
+    localStorage.setItem('productContent', productContent);
+
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+
+}
+function getImageDetail14() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN14').src;
+    const productContent = document.getElementById('product-content-14').text;
+    localStorage.setItem('productContent', productContent);
+
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+function getImageDetail15() {
+    // Get the color of the button on the first page
+    const imageAddress = document.getElementById('ImageN15').src;
+
+    const productContent = document.getElementById('product-content-15').text;
+    localStorage.setItem('productContent', productContent);
+    localStorage.setItem('imageAddress', imageAddress);
+
+    // Redirect to the second page
+    window.location.href = 'product-details-two.html';
+
+    console.log(imageAddress);
+
+}
+// Function to set the background color of the second page
+function setImage() {
+    // Get the color from local storage
+    const imageAddressNewPage = localStorage.getItem('imageAddress');
+    const productContent = localStorage.getItem('productContent');
+
+    // Set the background color of the second page
+    document.getElementById('newImage').src = imageAddressNewPage;
+    document.getElementById('productContent').innerHTML =  "کد " + productContent;
+
+}
